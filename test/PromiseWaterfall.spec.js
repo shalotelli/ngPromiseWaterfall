e95c17f21b0090c159850f690f7887005dcb696a
Sha <sha@responsiveshops.com>
2015-12-19 18:27:13 -0500

    add more tests

--- test/PromiseWaterfall.spec.js
+++ test/PromiseWaterfall.spec.js
@@ -142,4 +142,135 @@ it('Should resolve the first promise and reject the second, then fallback to onW
     expect(onWaterfallSuccess).not.toHaveBeenCalled();
     expect(onWaterfallError).toHaveBeenCalled();
   });
+
+  it('Should return a rejected promise if an array is not passed', function () {
+    var promises = [],
+        firstMethod,
+        secondMethod,
+        onWaterfallSuccess,
+        onWaterfallError;
+
+    for (var promiseCount=0;promiseCount<2;promiseCount++) {
+      promises.push($q.defer());
+    }
+
+    firstMethod = jasmine.createSpy('firstMethod').and.returnValue(promises[0].promise);
+    secondMethod = jasmine.createSpy('secondMethod').and.returnValue(promises[1].promise);
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall(firstMethod, secondMethod)
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).not.toHaveBeenCalled();
+    expect(onWaterfallError).toHaveBeenCalled();
+  });
+
+  it('Should return a rejected promise if the first element of the array is not a function', function () {
+    var onWaterfallSuccess,
+        onWaterfallError;
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall([ 1, 2 ])
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).not.toHaveBeenCalled();
+    expect(onWaterfallError).toHaveBeenCalled();
+  });
+
+  it('Should return a resolved promise ther array only has one element', function () {
+    var deferred,
+        firstMethod,
+        onWaterfallSuccess,
+        onWaterfallError;
+
+    deferred = $q.defer();
+
+    firstMethod = jasmine.createSpy('firstMethod').and.returnValue(deferred.promise);
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall([ firstMethod ])
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    deferred.resolve();
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).toHaveBeenCalled();
+    expect(onWaterfallError).not.toHaveBeenCalled();
+  });
+
+  it('Should return a rejected promise if the only element of the array is not a function', function () {
+    var onWaterfallSuccess,
+        onWaterfallError;
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall([ 1 ])
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).not.toHaveBeenCalled();
+    expect(onWaterfallError).toHaveBeenCalled();
+  });
+
+  fit('Should return a return promise if first function does not return a promise', function () {
+    var firstMethod,
+        onWaterfallSuccess,
+        onWaterfallError;
+
+    firstMethod = jasmine.createSpy('firstMethod').and.returnValue('not a promise');
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall([ firstMethod ])
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).toHaveBeenCalled();
+    expect(onWaterfallError).not.toHaveBeenCalled();
+  });
+  
+  it('Should return a rejected promise if a function does not return a promise', function () {
+    var deferred,
+        firstMethod,
+        secondMethod,
+        onWaterfallSuccess,
+        onWaterfallError;
+
+    deferred = $q.defer();
+
+    firstMethod = jasmine.createSpy('firstMethod').and.returnValue(deferred.promise);
+    secondMethod = jasmine.createSpy('secondMethod').and.returnValue(true);
+
+    onWaterfallSuccess = jasmine.createSpy('onWaterfallSuccess');
+    onWaterfallError = jasmine.createSpy('onWaterfallError');
+
+    new PromiseWaterfall(firstMethod, secondMethod)
+      .then(onWaterfallSuccess)
+      .catch(onWaterfallError);
+
+    deferred.resolve();
+    $rootScope.$apply();
+
+    expect(onWaterfallSuccess).not.toHaveBeenCalled();
+    expect(onWaterfallError).toHaveBeenCalled();
+  });
 });
